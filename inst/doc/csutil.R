5cbc2806cad24ec4ff356d9f48f7253228c19c86
Richard Aubrey White <hello@rwhite.no>
2023-04-25 06:10:02 +0000

    version 2023.4.25

--- inst/doc/csutil.R
+++ inst/doc/csutil.R
@@ -36,3 +36,19 @@ csutil::is_all_list_elements_null_or_list(list(list(), NULL))
 csutil::is_all_list_elements_null_or_fully_named_list(list(list(), NULL))
 csutil::is_all_list_elements_null_or_fully_named_list(list(list("a" = 1), NULL))
 
+## -----------------------------------------------------------------------------
+input <- rep(seq(as.Date("2000-01-01"), as.Date("2020-01-01"), 1), 1000)
+a1 <- Sys.time()
+z <- format(input, "%Y")
+a2 <- Sys.time()
+a2 - a1
+
+b1 <- Sys.time()
+z <- csutil::apply_fn_via_hash_table(
+  input,
+  format,
+  "%Y"
+)
+b2 <- Sys.time()
+b2 - b1
+
